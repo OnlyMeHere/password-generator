d0db12ec2b3eba30e3c47b3126f28524f15a90ee
OnlyMeHere <bennett45.j.m@gmail.com>
2022-06-22 19:11:08 -0700

    add arrays in JS

---
+++ README.md
@@ -0,0 +1,5 @@
+## password-generator
+
+This site was created to serve an emplyees need to generate a strong password. It works in the browser and can accommodate various criteria that secure password require.
+ 
+
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,17 @@
 // Assignment Code
+
+const lowerc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const upperc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","w","X","Y","Z"]
+const numbers = ["1","2","3","4","5","6","6","7","8","9","0"]
+const special = ["!","#","$","%","^","&","*","(",")","_","-","?"]
+
+
+
 var generateBtn = document.querySelector("#generate");
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +21,16 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword() {
+
+
+
+
+}
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
