f4bba0e7c7cad73cff6a25db4afd80788b22bf23
OnlyMeHere <bennett45.j.m@gmail.com>
2022-06-23 17:45:24 -0700

    linked CSS

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,26 @@ const upperc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
 const numbers = ["1","2","3","4","5","6","6","7","8","9","0"]
 const special = ["!","#","$","%","^","&","*","(",")","_","-","?"]
 
-
+// When I click the button to Generate Password, I'm presented with a 
+// series of Prompts for Password criteria
 
 var generateBtn = document.querySelector("#generate");
+
+// Then a Prompt askes for what length the password should be
+// between the limits of ( at least 8 no more than 128 characters )
+
+
+
+//  Then a Prompt asks for character types to include in the password
+// and alerts give me opions wheather or not to include the following
+// Lowercase, Uppercase, Numeric, and/or Special charactors.
+
+
+
+// Then user input should be validated and at least one charactors type selected
+
+
+// Then the password is generated and displayed in an alert of written to the page
 
 
 
@@ -24,7 +41,7 @@ function writePassword() {
 
 function generatePassword() {
 
-
+    
 
 
 }
